d54d1c66c66c9f6bca54640f9617f4ca921ebd0c
zpp0196 <794970247@qq.com>
2017-11-26 12:03:57 +0800

    init Commit

---
+++ app/src/main/java/me/zpp0196/FHelper/Hooks.java
@@ -0,0 +1,103 @@
+package me.zpp0196.FHelper;
+
+/**
+ * Created by zpp0196 on 2017/11/25.
+ */
+
+import android.content.Context;
+import android.net.Uri;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class Hooks implements IXposedHookLoadPackage {
+
+    private static XC_LoadPackage.LoadPackageParam loadPackageParam;
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        loadPackageParam = lpparam;
+        try {
+            patchcode();
+        } catch (ClassNotFoundException e) {
+            XposedBridge.log(e);
+        }
+    }
+
+    private void patchcode() throws ClassNotFoundException {
+
+        if (loadPackageParam.packageName.equals("com.meizu.customizecenter")) {
+
+            XposedBridge.log("crack by coderstory");
+            XposedBridge.log("build by zpp0196");
+
+            //device_states | doCheckState
+            //6.6.1
+            findAndHookMethod("com.meizu.customizecenter.utils.ak", loadPackageParam.classLoader, "h", Context.class, XC_MethodReplacement.returnConstant(0));
+            //6.7.0
+            findAndHookMethod("com.meizu.customizecenter.h.al", loadPackageParam.classLoader, "h", Context.class, XC_MethodReplacement.returnConstant(0));
+            findAndHookMethod("com.meizu.statsapp.util.Utils", loadPackageParam.classLoader, "isRoot", Context.class, XC_MethodReplacement.returnConstant(false));
+
+            //resetToSystemTheme
+            // 6.0.7 6.1.0 6.2.0 6.3.2
+            findAndHookMethod("com.meizu.customizecenter.common.theme.common.theme.a", loadPackageParam.classLoader, "e", XC_MethodReplacement.returnConstant(false));
+            //6.9.0
+            findAndHookMethod("com.meizu.customizecenter.common.theme.common.theme.b", loadPackageParam.classLoader, "a", XC_MethodReplacement.returnConstant(false));
+
+            //data/data/com.meizu.customizecenter/font/   system_font
+            //6.0.7 6.1.0 6.2.0
+            findAndHookMethod("com.meizu.customizecenter.common.font.FontManager", loadPackageParam.classLoader, "a", XC_MethodReplacement.returnConstant(true));
+            //6.7.0
+            findAndHookMethod("com.meizu.customizecenter.common.font.c", loadPackageParam.classLoader, "a", XC_MethodReplacement.returnConstant(true));
+            //6.8.0
+            findAndHookMethod("com.meizu.customizecenter.common.font.c", loadPackageParam.classLoader, "e", XC_MethodReplacement.returnConstant(""));
+            findAndHookMethod("com.meizu.customizecenter.common.font.f", loadPackageParam.classLoader, "b", XC_MethodReplacement.returnConstant(false));
+
+            //主题混搭 ThemeContentProvider query Unknown URI
+            findAndHookMethod("com.meizu.customizecenter.common.dao.ThemeContentProvider", loadPackageParam.classLoader, "query", Uri.class, String[].class, String.class, String[].class, String.class, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+                    Object[] objs = param.args;
+                    String Tag = "(ITEMS LIKE";
+                    String Tag2 = "%zklockscreen;%";
+                    String Tag3 = "%com.meizu.flyme.weather;%";
+                    //XposedBridge.log("开始");
+                    boolean result = false;
+                    for (Object obj : objs) {
+                        // XposedBridge.log(obj==null?"":obj.toString());
+                        if (obj instanceof String && (((String) obj).contains(Tag) || obj.equals(Tag2) || obj.equals(Tag3))) {
+                            result = true;
+                        }
+                    }
+                    // XposedBridge.log("结束");
+                    if (result) {
+                        for (Object obj : objs) {
+                            if (obj instanceof String[]) {
+                                for (int j = 0; j < ((String[]) obj).length; j++) {
+                                    if (((String[]) obj)[j].contains("/storage/emulated/0/Customize/Themes")) {
+                                        ((String[]) obj)[j] = "/storage/emulated/0/Customize%";
+                                    } else if (((String[]) obj)[j].contains("/storage/emulated/0/Customize/TrialThemes")) {
+                                        ((String[]) obj)[j] = "NONE";
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    super.beforeHookedMethod(param);
+                }
+            });
+        }
+    }
+
+    private static void findAndHookMethod(String p1, ClassLoader lpparam, String p2, Object... parameterTypesAndCallback) {
+        try {
+            XposedHelpers.findAndHookMethod(p1, lpparam, p2, parameterTypesAndCallback);
+        } catch (Throwable localString3) {
+            XposedBridge.log(localString3);
+        }
+    }
+}
---
+++ app/src/main/java/me/zpp0196/FHelper/MainActivity.java
@@ -0,0 +1,16 @@
+package me.zpp0196.FHelper;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = findViewById(R.id.webview_main);
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
